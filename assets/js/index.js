b96121e3720f083bc606fcaa90f79d7f49d749cc
neowebor <neowebor@gmail.com>
2024-11-20 20:45:33 +0200

    Add basic react render example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,41 @@
 /*
+  React - точка входу в бібліотеку реакт (як document для DOM)
+
+  React.createElement(
+    type,
+    props,
+    ...children
+  ) - створює реактівський елемент
+
+  type - тип елемента
+  props - "атрибути" елемента
+  children - будь яка кількість елемента
+
+  ReactDOM - інтерфейс для роботи з віртуальним DOM реакту.
+
+  ReactDOM.createRoot(htmlElement) - створює кореневий елемент реакту в якому будуть відображені всі інші елементи
+*/
+
+/*
   Через ДОМ створити параграф з текстом, надати йому клас
   та відобразити на сторінці
 */
 
-const root = document.getElementById('root');
+// console.log(React);
 
-const paragraph = document.createElement('p');
+// const root = document.getElementById('root');
 
-paragraph.textContent = 'Lorem Ipsum';
-paragraph.classList.add('text');
+// const paragraph = document.createElement('p');
 
-root.append(paragraph);
+// paragraph.textContent = 'Lorem Ipsum';
+// paragraph.classList.add('text');
+
+// root.append(paragraph);
+
+const htmlContainer = document.getElementById('root');
+const root = ReactDOM.createRoot(htmlContainer);
+
+// в реакті класи заносятся в атрибут className
+const par = React.createElement('p', { className : 'text' }, 'Lorem Ipsum');
+
+root.render(par)
